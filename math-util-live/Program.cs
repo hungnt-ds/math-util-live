b4220524c1482d700c6e17ae7d10678c75ac5ff9
hungnt-ds <hungnt.dfs@gmail.com>
2024-03-02 20:31:48 +0700

    Lan dau lam chuyen ay - dua code len Server

---
+++ math-util-live/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace math_util_live
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
